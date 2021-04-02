26fdf333816de477bf6d204c39c1d1a09716dfcb
Riquelme Ciro <ciroriquelme05@gmail.com>
2021-04-02 03:03:27 -0300

    Lab 01 - Lab02 - Pedido

---
+++ src/main/java/utn/isi/dan/lab01/domain/EstadoPedido.java
@@ -0,0 +1,30 @@
+package utn.isi.dan.lab01.domain;
+
+public class EstadoPedido {
+
+	public EstadoPedido() {
+		
+	}
+	
+	public EstadoPedido(Integer id, String estado) {
+		super();
+		this.id = id;
+		this.estado = estado;
+	}
+	
+	private Integer id;
+	private String estado;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getEstado() {
+		return estado;
+	}
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+}
