d5eeaee06dce81b7f24ac7077e031e03779311cc
Julian Sanchez <juliansanchez@Julians-MacBook-Pro.local>
2021-08-17 17:24:00 -0300

    cross origin

--- src/main/java/utn/isi/dan/pedido/rest/PedidoRest.java
+++ src/main/java/utn/isi/dan/pedido/rest/PedidoRest.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -24,6 +25,7 @@ import utn.isi.dan.pedido.exception.BadRequestException;
 import utn.isi.dan.pedido.service.IPedidoService;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api/pedido")
 @Api(value = "PedidoRest")
 public class PedidoRest {
